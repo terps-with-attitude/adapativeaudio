29907b7d172153bae2237d6fdb276e30f8447ec2
Abhishek Velayudham <avelay@terpmail.umd.edu>
2017-02-18 18:04:30 -0500

    Latest version of splitting the audio

--- src/split_music.py
+++ src/split_music.py
@@ -4,61 +4,76 @@ import matplotlib.pyplot as plt
 import adaptfilt as adf
 from scipy.io.wavfile import write
 
-voice = wave.open("voice.wav", "r")
+voice = wave.open("musicvoice.wav", "r")
 music = wave.open("music.wav", "r")
 
 
 
-music.readframes(900000)
+full_e = np.array([])
+full_u = np.array([])
+full_d = np.array([])
 
-music_frames = music.readframes(80000)
-voice_frames = voice.readframes(160000)
+i = 0
 
-
-u = np.fromstring(music_frames, np.int16)
-u = np.float64(u)
-
-
-v= np.fromstring(voice_frames, np.int16)
-v = np.float64(v)
-
-d = u+v
+while i < 80: 
+	music_frames = music.readframes(10000)
+	voice_frames = voice.readframes(10000)
 
 
-# Apply adaptive filter
-M = 100  # Number of filter taps in adaptive filter
-step = 0.1  # Step size
-y, e, w = adf.nlms(u, d, M, step, returnCoeffs=True)
-#
-#plt.figure()
-#plt.title('u(n)')
-#plt.plot(u)
-#plt.grid()
-#plt.xlabel('Samples')
-#
-#plt.figure()
-#plt.title('v(n)')
-#plt.plot(v)
-#plt.ylim([-5500, 5000])
-#plt.grid()
-#plt.xlabel('Samples')
-#
-#plt.figure()
-#plt.title('e (n)')
-#plt.plot(e)
-#plt.grid()
-#plt.ylim([-5500, 5000])
-#plt.xlabel('Samples')
-#
-#
-#
-#plt.show()
+	u = np.fromstring(music_frames, np.int16)
+	u = np.float64(u)
+
+	print(i)
+
+	d = np.fromstring(voice_frames, np.int16)
+	d = np.float64(d)
 
 
-scaled = np.int16(e/np.max(np.abs(e)) * 32767)
+	# Apply adaptive filter
+	M = 20  # Number of filter taps in adaptive filter
+	step = 0.1  # Step size
+	y, e, w = adf.nlms(u, d, M, step, returnCoeffs=True)
 
-write('test.wav', 32000, scaled)
+	full_e = np.concatenate([full_e, e])
+	full_d = np.concatenate([full_d, d])
+	full_u = np.concatenate([full_u, u])
+	i += 1
 
-scaled_u = np.int16(u/np.max(np.abs(u)) * 32767)
 
-write('new_musc.wav', 64000, scaled_u)
+
+
+
+
+
+# plt.figure()
+# plt.title('Music (unaltered)')
+# plt.plot(full_u)
+# plt.grid()
+# plt.xlabel('Samples')
+
+# plt.figure()
+# plt.title('Mixed voice and music')
+# plt.plot(full_d)
+# plt.ylim([-30000, 30000])
+# plt.grid()
+# plt.xlabel('Samples')
+
+# plt.figure() 
+# plt.title('Predicted Voice obtained by LMS Algo.')
+# plt.plot(full_e)
+# plt.grid()
+# plt.ylim([-30000, 30000])
+# plt.xlabel('Samples')
+
+
+
+plt.show()
+
+
+scaled = np.int16(full_e/np.max(np.abs(full_e)) * 32767)
+
+write('e.wav', 32000, scaled)
+
+scaled_d = np.int16(full_d/np.max(np.abs(full_d)) * 32767)
+
+write('mixedtest.wav', 32000, scaled_d)
