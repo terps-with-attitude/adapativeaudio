2d9b728ce200e1d90485d347a26ee507fbdd7269
Affan <affan98@gmail.com>
2017-02-18 10:51:17 -0500

    Updated functions for gradual volume change

--- src/audio-ctrl-dir/volumeChange.py
+++ src/audio-ctrl-dir/volumeChange.py
@@ -1,6 +1,7 @@
 from subprocess import call, Popen, PIPE
+import time
 
-vol = 0
+defaultVol = 0
 
 def getSystemVol():
 	process = Popen(["amixer", "-D", "pulse", "sget", "Master"], stdout=PIPE)
@@ -12,16 +13,28 @@ def getSystemVol():
 	location = output.find("[") + 1
 	location2 = output.find("%")
 
-	vol = int(output[location : location2 ])
+	return (int(output[location : location2 ]))
 
 def lowerVol(percentLowered = 10):
-	percentLowered = str(percentLowered)
-	call(["amixer", "-D", "pulse", "sset", "Master", percentLowered + "%" + "-"])
+	#percentLowered = str(percentLowered)
+	for x in range(0, percentLowered + 1):
+		call(["amixer", "-D", "pulse", "sset", "Master", "1%-"])
+		time.sleep(.05)
 
-def setVol(volVal = vol):
-	volVal = str(volVal)
-	call(["amixer", "-D", "pulse", "sset", "Master", volVal + "%"])
+def setVol(volVal):
+
+	currentVol = getSystemVol()
+	print(currentVol)
+	print(volVal)
+
+	for x in range(0, volVal - currentVol + 1):
+		call(["amixer", "-D", "pulse", "sset", "Master","1%+"])
+		time.sleep(.05)
 
 
 
-getSystemVol()
+defaultVol = getSystemVol()
+print("defualt vol is", defaultVol)
+lowerVol()
+time.sleep(5)
+setVol(defaultVol)
