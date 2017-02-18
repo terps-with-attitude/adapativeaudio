38f7c0d3906685e49e6ea677a432a4de54390ac2
Affan <affan98@gmail.com>
2017-02-18 02:39:23 -0500

    Functions for raising and lowering system volume

---
+++ src/audio-ctrl-dir/volumeChange.py
@@ -0,0 +1,30 @@
+from subprocess import call, Popen, PIPE
+import time
+
+process = Popen(["amixer", "-D", "pulse", "sget", "Master"], stdout=PIPE)
+(output, err) = process.communicate()
+exit_code = process.wait()
+
+output = str(output)
+
+location = output.find("[") + 1
+location2 = output.find("%")
+
+
+
+vol = int(output[location : location2 ])
+
+
+def lowerVol(percentLowered = 10):
+	percentLowered = str(percentLowered)
+	call(["amixer", "-D", "pulse", "sset", "Master", percentLowered + "%" + "-"])
+
+def setVol(volVal = vol):
+	volVal = str(volVal)
+	call(["amixer", "-D", "pulse", "sset", "Master", volVal + "%"])
+
+
+lowerVol()
+time.sleep(10)
+setVol()
+
